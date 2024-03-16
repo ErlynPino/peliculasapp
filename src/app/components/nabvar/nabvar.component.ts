84f820ad567a50f60cb5615e87c0af93295bfee1
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-15 19:13:18 -0500

    Proyecto aPi Peliculas con TMDB

---
+++ src/app/components/nabvar/nabvar.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nabvar.component.html',
+  styleUrl: './nabvar.component.css'
+})
+export class NabvarComponent {
+
+}
