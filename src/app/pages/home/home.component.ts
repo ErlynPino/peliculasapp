5d0c29aff6c541e63fb62e42d28cb889becaa641
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-19 19:27:00 -0500

    infinity scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera.interface';
 import { SlideshowComponent } from '../../components/slideshow/slideshow.component';
@@ -15,6 +15,17 @@ import { PeliculasPosterComponent } from '../../components/peliculas-poster/peli
 export class HomeComponent  implements OnInit{
 
   movies: Movie[]=[];
+  loadedMoviesIds = new Set<number>();
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(){
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + 1000;
+    const max = (document.documentElement.scrollHeight || document.body.scrollHeight);
+
+    if(pos > max){
+      this.loadMoreMovies();
+    }
+  }
 
   constructor(private peliculasSvc: PeliculasService){}
 
@@ -25,7 +36,20 @@ export class HomeComponent  implements OnInit{
   loadMovies(){
     this.peliculasSvc.getCartelera().subscribe(res=>{
       this.movies = res;
+      this.updateLoadedMoviesIds(res[0].id);
     })
+  }
+
+  loadMoreMovies(){
+    this.peliculasSvc.getCartelera().subscribe(res=>{
+      const newMovies = res.filter(movie=> !this.loadedMoviesIds.has(movie.id));
+      this.movies.push(...newMovies);
+      this.updateLoadedMoviesIds(newMovies[0].id);
+    })
+  }
+
+  updateLoadedMoviesIds(id: number){
+    this.movies.forEach(movie =>this.loadedMoviesIds.add(movie.id))
   }
 
 }
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { CarteleraResponse, Movie } from '../interfaces/cartelera.interface';
 
 
@@ -13,12 +13,24 @@ export class PeliculasService {
   private URL= 'https://api.themoviedb.org/3'
   private apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGI4Zjc4ZjY5ZWFiOTBjZmQ0MGVhZGI5YWZkNzE4ZCIsInN1YiI6IjY1ZjRkOGQxMTY4NGY3MDE3ZGJiZTU4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1PElIJas4lnJRW5KO2B9tV1VqmPLrN8H9bUvHXYUkFY'
   private headers = {Authorization:`Bearer ${this.apiKey}`};
+  private carteleraPage = 1;
+  public cargando = false;
 
   constructor(private http:HttpClient) { }
 
   getCartelera():Observable<Movie[]>{
-    return this.http.get<CarteleraResponse>(`${this.URL}/movie/now_playing?language=es-ES&page=1`,{headers:this.headers}).pipe(
-      map((response:any)=> response.results)
+
+    if (this.cargando) {
+      return of([]);
+    }
+    this.cargando = true;
+
+    return this.http.get<CarteleraResponse>(`${this.URL}/movie/now_playing?language=es-ES&page=${this.carteleraPage}`,{headers:this.headers}).pipe(
+      map((response:any)=> response.results),
+      tap(()=> {
+        this.carteleraPage+=1;
+        this.cargando = false;
+      })
     )
   }
 }
