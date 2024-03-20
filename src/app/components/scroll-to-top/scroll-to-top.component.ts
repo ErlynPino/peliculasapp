e4fb1684420c0dedea701e61494774c0dbc4cb79
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-20 13:40:29 -0500

    scroll

--- src/app/components/scroll-to-top/scroll-to-top.component.ts
+++ src/app/components/scroll-to-top/scroll-to-top.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-to-top',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './scroll-to-top.component.html',
   styleUrl: './scroll-to-top.component.css'
 })
 export class ScrollToTopComponent {
+  windowScrolled = false;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  @HostListener('window:scroll', [])
+    onWindowScroll() {
+        if (window.screenY || this.document.documentElement.scrollTop || this.document.body.scrollTop > 100) {
+          this.windowScrolled = true;
+        } else if(this.windowScrolled && window.screenY || this.document.documentElement.scrollTop || this.document.body.scrollTop < 10) {
+          this.windowScrolled = false;
+        }
+    }
+
+    scrollTop(){
+      this.document.body.scrollTop = 0;
+      this.document.documentElement.scrollTop = 0;
+    }
 
 }
+
+
