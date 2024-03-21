22c19212cf3eacd1892be873c91f44dff1e455f0
ErlynPinoTorres <98849964+ErlynPinoTorres@users.noreply.github.com>
2024-03-21 18:18:36 -0500

    actores

--- src/app/components/cast-slide-show/cast-slide-show.component.ts
+++ src/app/components/cast-slide-show/cast-slide-show.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { Cast } from '../../interfaces/credits.interface';
+import Swiper from 'swiper';
+import { PipesModule } from "../../pipes/pipes.module";
 
 @Component({
-  selector: 'app-cast-slide-show',
-  standalone: true,
-  imports: [],
-  templateUrl: './cast-slide-show.component.html',
-  styleUrl: './cast-slide-show.component.css'
+    selector: 'app-cast-slide-show',
+    standalone: true,
+    templateUrl: './cast-slide-show.component.html',
+    styleUrl: './cast-slide-show.component.css',
+    imports: [CommonModule, PipesModule]
 })
-export class CastSlideShowComponent {
+export class CastSlideShowComponent implements AfterViewInit{
+
+  @Input() cast?:Cast[];
+
+  ngAfterViewInit() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 4,
+      freeMode: true,
+      spaceBetween: 15,
+    })
+  }
 
 }
